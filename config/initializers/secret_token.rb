23c814806c5309e706fca851d0cad399fac072c7
Tyler McElhaney <tmac@zcodemedia.com>
2013-10-16 11:55:39 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Publio::Application.config.secret_token = 'e030f9070819897b5c3fcd44fc7842e31174a5c40d13ab058326cc13554db56ce75397d80a9afb70afabbd15c59ce58dd01159455e1a6a088a956c982d9445d0'
